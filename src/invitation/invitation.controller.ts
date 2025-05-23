5a7cd568a59d358e94f09431baf4f5156403be97
Arodrigow <rodrigow_@hotmail.com.br>
2025-05-23 16:29:22 -0300

    Added jwt and role based guards on create invitation

--- src/invitation/invitation.controller.ts
+++ src/invitation/invitation.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { InvitationService } from './invitation.service';
-import { Prisma } from '@prisma/client';
+import { Prisma, Role } from '@prisma/client';
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard';
+import { RolesGuard } from '@/auth/roles/roles.guard';
+import { Roles } from '@/auth/roles/roles.decorator';
 
 @Controller('invitation')
 export class InvitationController {
@@ -8,6 +11,8 @@ export class InvitationController {
         private readonly invitation: InvitationService
     ){}
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
     @Post()
     createInvitation(@Body() data: Prisma.InvitationCreateInput){
         return this.invitation.createInvitation(data.email);
