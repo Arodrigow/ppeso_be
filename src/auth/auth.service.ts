4cbc5b47edd4dbd82dd43cccc09c4cf36225d5f4
Arodrigow <rodrigow_@hotmail.com.br>
2025-05-23 16:23:23 -0300

    Added auth infra

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService) { }
+
+    async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.userService.getUserByEmail(email);
+        if (user && await bcrypt.compare(pass, user.password)) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+     async login(user: any) {
+        const payload = { username: user.email, sub: user.id, role: user.role };
+        return {
+            access_token: this.jwtService.sign(payload, {
+                expiresIn: '24h',
+            }),
+        };
+    }
+}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '@/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly prisma: PrismaService,) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get<string>('JWT_SECRET', 'secret_key'), // Use a default secret key for development
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.prisma.user.findUnique({ where: { id: payload.sub } });
+    return user; // attaches to request.user
+  }
+}
