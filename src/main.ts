b7859d830e82541c1568cb0d1fefc32fd442fa14
Arodrigow <rodrigow_@hotmail.com.br>
2025-06-25 10:18:57 -0300

    Added keep alive ping

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 import fastifyRateLimit from '@fastify/rate-limit';
 import fastifyHelmet from '@fastify/helmet';
 import { AllExceptionsFilter } from './utils/all-exceptions.filter';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -33,7 +34,16 @@ async function bootstrap() {
 
   // 🟢 This is critical
   await app.init();
-
+  setInterval(async () => {
+    try {
+      const prisma = app.get(PrismaService);
+      await prisma.$queryRaw`SELECT 1`; // lightweight ping
+      console.log('[DB Ping] Keep-alive query sent');
+    } catch (error) {
+      console.error('[DB Ping] Failed:', error);
+    }
+  }, 10 * 60 * 1000); // every 10 minutes
+  
   // Then start server
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
